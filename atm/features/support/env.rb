24f4cee1e0c53e89d44555a056cd5e0a41b52a94
Ben <ben@frshwtr.uk>
2021-05-31 23:11:09 +0100

    introducing ui tests

--- atm/features/support/env.rb
+++ atm/features/support/env.rb
@@ -1,1 +1,5 @@
 require_relative '../../lib/nice_bank'
+
+require 'capybara/cucumber'
+Capybara.app = Sinatra::Application
+Sinatra::Application.set :environment, :test
--- atm/lib/nice_bank.rb
+++ atm/lib/nice_bank.rb
@@ -31,3 +31,29 @@ class Teller
         @cash_slot.dispense(amount)
     end
 end
+
+require 'sinatra'
+
+get '/' do
+    %{
+        <html>
+            <body>
+                <form action="withdraw" method="post>
+                    <label for="amount">Amount</label>
+                    <input type="text" id="amount" name="amount">
+                    <button type="submit">Withdraw</button>
+                </form>
+            </body>
+        </html>
+    }
+end
+
+set :cash_slot, CashSlot.new
+set :account do
+    fail 'Account has not been set'
+end
+
+post '/withdraw' do
+    teller = Teller.new(settings.cash_slot)
+    teller.withdraw_from(settings.account, params[:amount].to_i)
+end
